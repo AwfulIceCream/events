6faa5622e70debc4aad4b71a620d9ecb84ce122e
Andrii <andriymyrosh@gmail.com>
2023-11-30 22:20:45 +0200

    Add models

--- app.py
+++ app.py
@@ -2,11 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/api/v1/hello-world-20')
-def hello_world():
-    return 'Hello World, 20'
-
-
 if __name__ == '__main__':
     app.run()
