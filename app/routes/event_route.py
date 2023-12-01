21e1775aa32adc1187399ae369792d1e9bce4f47
Andrii <andriymyrosh@gmail.com>
2023-12-01 14:56:57 +0200

    Add event route

---
+++ app/routes/event_route.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from app.models.db import db
+from app.models import EventModel
+from app.models import UserModel
+from schemas import EventSchema, UpdateEventSchema, UserGetSchema
+
+blp = Blueprint('Events', __name__, url_prefix='/events', description='Operations on events')
+
+
+@blp.route('/')
+class EventsList(MethodView):
+    @blp.response(200, EventSchema(many=True))
+    def get(self):
+        events = EventModel.query.all()
+        return EventSchema(many=True).dump(events)
+
+    @jwt_required()
+    @blp.arguments(EventSchema)
+    @blp.response(201, EventSchema)
+    def post(self, new_data):
+        user_id = get_jwt_identity()
+        user = UserModel.query.get_or_404(user_id)
+        datetime_object = datetime.strptime(new_data["date"], "%Y-%m-%dT%H:%M:%S")
+        if not user:
+            abort(404, message="User not found.")
+        event = EventModel(title=new_data["title"], description=new_data["description"], date=datetime_object,
+                           creator=user)
+        db.session.add(event)
+        db.session.commit()
+        return EventSchema().dump(event), 201
+
+
+@blp.route('/<int:event_id>')
+class EventResource(MethodView):
+    @jwt_required()
+    @blp.response(200, EventSchema)
+    def get(self, event_id):
+        event = EventModel.query.get_or_404(event_id)
+        if not event:
+            abort(404, message="Event not found.")
+        return EventSchema().dump(event)
+
+    @blp.arguments(UpdateEventSchema)
+    @blp.response(200, UpdateEventSchema)
+    @jwt_required()
+    def put(self, update_data, event_id):
+        event = EventModel.query.get_or_404(event_id)
+        if not event:
+            abort(404, message="Event not found.")
+        if event.creator.id != get_jwt_identity():
+            abort(403, message="User not authorized to edit this event.")
+
+        for key, value in update_data.items():
+            setattr(event, key, value)
+        db.session.add(event)
+        db.session.commit()
+        return EventSchema().dump(event)
+
+    @jwt_required()
+    @blp.response(204)
+    def delete(self, event_id):
+        event = EventModel.query.get_or_404(event_id)
+        if not event:
+            abort(404, message="Event not found.")
+        if event.creator.id != get_jwt_identity():
+            abort(403, message="User not authorized to delete this event.")
+        db.session.delete(event)
+        db.session.commit()
+        return '', 204
+
+
+@blp.route('/<int:event_id>/attend/<int:user_id>')
+class AttendEventResource(MethodView):
+    @jwt_required()
+    @blp.response(200, EventSchema)
+    def post(self, event_id, user_id):
+        event = EventModel.query.get_or_404(event_id)
+        if not event:
+            abort(404, message="Event not found.")
+
+        user = UserModel.query.get_or_404(user_id)
+        if not user:
+            abort(404, message="User not found.")
+
+        event.attendees.append(user)
+
+        db.session.add(event)
+        db.session.commit()
+
+        return EventSchema().dump(event)
+
+
+@blp.route('/<int:event_id>/attendees')
+class EventAttendeesResource(MethodView):
+    @jwt_required()
+    @blp.response(200, UserGetSchema(many=True))
+    def get(self, event_id):
+        event = EventModel.query.get_or_404(event_id)
+        if not event:
+            abort(404, message="Event not found.")
+
+        attendees = event.attendees
+
+        return UserGetSchema(many=True).dump(attendees)
