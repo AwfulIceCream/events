a027638275cc9cb912cd7a73fb59f373196a2ed0
Andrii <andriymyrosh@gmail.com>
2023-12-01 09:47:10 +0200

    Add models

---
+++ models/user.py
@@ -0,0 +1,10 @@
+from models.attendees import attendees
+from models.db import db
+
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    created_events = db.relationship('Event', backref='creator', lazy='dynamic')
+    attending = db.relationship('Event', secondary=attendees, backref=db.backref('attendees', lazy='dynamic'))
