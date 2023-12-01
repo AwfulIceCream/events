5b5842bcbc6c84cd5c905400a1a070af93dde89d
Andrii <andriymyrosh@gmail.com>
2023-12-01 14:58:22 +0200

    /

--- models/language.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-
-from base.base import Base
-
-
-class Language(Base):
-    __tablename__ = 'language'
-    language_id = Column(Integer, primary_key=True)
-    name = Column(String)
