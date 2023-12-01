b61284e50a43b591ba923e014e6098d6a3443f77
Andrii <andriymyrosh@gmail.com>
2023-12-01 10:08:25 +0200

    Update schemas

--- schemas.py
+++ schemas.py
@@ -2,26 +2,26 @@ from marshmallow import Schema, fields, post_load
 from models import UserModel, EventModel
 
 
-class UserSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str()
-    email = fields.Str()
-    password = fields.Str(load_only=True)
-    created_events = fields.Nested('EventSchema', many=True, exclude=('creator',))
-    attending = fields.Nested('EventSchema', many=True)
-
-    @post_load
-    def make_user(self, data, **kwargs):
-        return UserModel(**data)
-
-
 class EventSchema(Schema):
     id = fields.Int(dump_only=True)
     title = fields.Str()
     description = fields.Str()
     date = fields.DateTime()
-    creator = fields.Nested(UserSchema, only=('id', 'name'))
+    creator = fields.Nested(lambda: UserSchema(only=('id', 'name')))
 
     @post_load
     def make_event(self, data, **kwargs):
         return EventModel(**data)
+
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+    email = fields.Str()
+    password = fields.Str(load_only=True)
+    created_events = fields.Nested(EventSchema(), many=True, exclude=('creator',))
+    attending = fields.Nested(EventSchema(), many=True)
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        return UserModel(**data)
