2f3b4f63806101e08774ce72ae497bc1685bc224
Andrii <andriymyrosh@gmail.com>
2023-12-01 15:56:04 +0200

    Create config

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,23 @@
-from flask import Flask, jsonify
-from flask_smorest import Api
-from flask_jwt_extended import JWTManager
-from blocklist import BLOCKLIST
-from flask_migrate import Migrate
-
 from .routes import AuthBlueprint, EventBlueprint, UserBlueprint
 from .models.db import db
 
 
-def create_app():
+from flask import Flask, jsonify
+from flask_smorest import Api
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+
+from config import DevelopmentConfig
+
+
+def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
-    app.config["API_TITLE"] = "Events REST API"
-    app.config["API_VERSION"] = "v1"
-    app.config["OPENAPI_VERSION"] = "3.0.3"
-    app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger"
-    app.config[
-        "OPENAPI_SWAGGER_UI_URL"
-    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config.from_object(config_class)
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    migrate = Migrate(app, db)
     api = Api(app)
 
-    migrate = Migrate(app, db)
-
-    app.config["JWT_SECRET_KEY"] = "andrii"
-    jwt = JWTManager(app)
-
-    config_jwt(jwt)
-
-    app.config['SECRET_KEY'] = '9c9c2ff463c47cea625cf6652b9b2043'
+    config_jwt(app)
 
     api.register_blueprint(AuthBlueprint)
     api.register_blueprint(EventBlueprint)
@@ -42,57 +26,23 @@ def create_app():
     return app
 
 
-def config_jwt(jwt):
-    # for JWT tokens
-    @jwt.token_in_blocklist_loader
-    def check_if_token_in_blocklist(jwt_header, jwt_payload):
-        return jwt_payload["jti"] in BLOCKLIST
+def config_jwt(app):
+    jwt = JWTManager(app)
 
     @jwt.expired_token_loader
-    def expired_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify({"message": "The token has expired.", "error": "token_expired"}),
-            401,
-        )
+    def expired_token_callback():
+        response = jsonify({"message": "The token has expired"})
+        response.status_code = 401
+        return response
 
     @jwt.invalid_token_loader
-    def invalid_token_callback(error):
-        return (
-            jsonify(
-                {"message": "Signature verification failed.", "error": "invalid_token"}
-            ),
-            401,
-        )
+    def invalid_token_callback():
+        response = jsonify({"message": "Signature verification failed"})
+        response.status_code = 401
+        return response
 
     @jwt.unauthorized_loader
-    def missing_token_callback(error):
-        return (
-            jsonify(
-                {
-                    "description": "Request does not contain an access token.",
-                    "error": "authorization_required",
-                }
-            ),
-            401,
-        )
-
-    @jwt.needs_fresh_token_loader
-    def token_not_fresh_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {
-                    "description": "The token is not fresh.",
-                    "error": "fresh_token_required",
-                }
-            ),
-            401,
-        )
-
-    @jwt.revoked_token_loader
-    def revoked_token_callback(jwt_header, jwt_payload):
-        return (
-            jsonify(
-                {"description": "The token has been revoked.", "error": "token_revoked"}
-            ),
-            401,
-        )
+    def missing_token_callback():
+        response = jsonify({"message": "Request does not contain an access token"})
+        response.status_code = 401
+        return response
---
+++ config.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Config:
+    API_TITLE = "Events REST API"
+    API_VERSION = "v1"
+    OPENAPI_VERSION = "3.0.3"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_SWAGGER_UI_PATH = "/swagger"
+    OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
+    JWT_SECRET_KEY = "andrii"
+    SECRET_KEY = "9c9c2ff463c47cea625cf6652b9b2043"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    JWT_SECRET_KEY = "andrii"
+    SECRET_KEY = "9c9c2ff463c47cea625cf6652b9b2043"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
