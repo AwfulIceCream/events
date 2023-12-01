af5df9664d6a93f5ca650aed4aae07059e3e11a7
Andrii <andriymyrosh@gmail.com>
2023-12-01 14:58:22 +0200

    /

--- models/genre.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-
-from base.base import Base
-
-
-class Genre(Base):
-    __tablename__ = 'genre'
-    genre_id = Column(Integer, primary_key=True)
-    genre_name = Column(String)
