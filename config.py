6557074213af1adc7bdba98a913f932fbc664426
Andrii <andriymyrosh@gmail.com>
2023-12-04 22:02:49 +0200

    Make migration

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     OPENAPI_SWAGGER_UI_PATH = "/swagger"
     OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
-    SQLALCHEMY_TRAЯCK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     PROPAGATE_EXCEPTIONS = True
     JWT_SECRET_KEY = "andrii"
     SECRET_KEY = "9c9c2ff463c47cea625cf6652b9b2043"
